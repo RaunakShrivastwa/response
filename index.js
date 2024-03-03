7264e0c23e9324711f0cd1312f2005dabe9e9020
RaunakShrivastwa <kumarraunak4201@gmail.com>
2024-03-03 15:22:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express= require('express');
+
+const app =express();
+const PORT = 3000
+
+app.use(express.json())
+
+app.get("/home",(req,res)=>{
+    return res.json("Home PAge")
+})
+
+
+
+
+
+
+
+
+app.listen(PORT,(err)=>{
+    if(err){
+        return console.log("There is Error");
+    }
+
+    console.log("Servre is Running");
+})
